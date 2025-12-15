e82babfeaf9ce6095375cb2fb724f4617da60151
Sakura-winter <sumitkum1819@gmail.com>
2025-12-15 22:52:18 +0530

    chore: initialize Vite React app with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Popular from './pages/Popular'
+import TopRated from './pages/TopRated'
+import Upcoming from './pages/Upcoming'
+import MovieDetails from './pages/MovieDetails'
+import Search from './pages/Search'
+import Favorites from './pages/Favorites'
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/top-rated" element={<TopRated />} />
+        <Route path="/upcoming" element={<Upcoming />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Layout>
+  )
+}
+
+export default App
+
